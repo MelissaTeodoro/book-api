3a54d4ab378473303a4e2f42afc3f88a92097063
Melissa Teodoro <melmelissa1936@gmail.com>
2021-01-24 20:51:29 -0300

    feat: creating repositories interfaces

---
+++ src/main/java/com/read/book/api/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package com.read.book.api.repositories;
+
+import com.read.book.api.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+}
---
+++ src/main/java/com/read/book/api/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.read.book.api.repositories;
+
+import com.read.book.api.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
